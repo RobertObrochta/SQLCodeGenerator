b10404c6295bebabf27a1fb7fc55622182db91e5
George Bjork <georgebjork@outlook.com>
2022-10-31 18:01:29 -0700

    custom modal button colors

--- src/Components/AddTableModal.js
+++ src/Components/AddTableModal.js
@@ -3,20 +3,22 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
+import './Stylesheets/AddTableModal.css';
+
 
 function AddTableModal(props){
 
     return(
-        <Modal  {...props}>
+        <Modal  {...props} dialogClassName="custom-modal">
             <Modal.Header closeButton>
                 <Modal.Title>Add a New Table</Modal.Title>
             </Modal.Header>
             <Modal.Body>Hello! This will where data can be added for a table!</Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={props.onHide}>
+                <Button variant="secondary" className="cancel-btn" onClick={props.onHide}>
                     Close
                 </Button>
-                <Button variant="primary" onClick={props.onSubmit}>
+                <Button variant="primary" className="submit-btn" onClick={props.onSubmit}>
                     Add Table
                 </Button>
             </Modal.Footer>
